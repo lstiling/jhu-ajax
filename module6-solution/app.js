825961b766f7201df513d73cde85b6d9fba41ff0
Leah Stiling <Leah.Stiling@jhuapl.edu>
2018-10-06 21:13:41 -0400

    Add function to filter empty items out of the lunch menu list

--- module6-solution/app.js
+++ module6-solution/app.js
@@ -18,17 +18,28 @@ function LunchCheckController($scope, $filter)
 
 function getLunchMessage(lunchItems)
 {
-  if(lunchItems == "")
+  var pureLunchItems = filterLunchItems(lunchItems);
+  if(pureLunchItems == "")
   {
     return "Please enter data first.";
   }
-  else if(lunchItems.length <= 3)
+  else if(pureLunchItems.length <= 3)
   {
     return "Enjoy!";
   }
-  else if(lunchItems.length > 3)
+  else if(pureLunchItems.length > 3)
   {
     return "Too Much!";
   }
 }
+
+function filterLunchItems(lunchItems)
+{
+  var pureLunchItems = lunchItems.filter(function(element) {
+    if(element != " ") return element;
+  });
+
+  return pureLunchItems;
+}
+
 })();
