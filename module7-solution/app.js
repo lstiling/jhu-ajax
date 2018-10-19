b87d490187a0b16f6d13a614649ac367f775dec5
Leah Stiling <Leah.Stiling@jhuapl.edu>
2018-10-19 16:47:45 -0400

    Add buy and bought controllers and lists

---
+++ module7-solution/app.js
@@ -0,0 +1,30 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController .$inject = ['$scope', '$filter'];
+function ToBuyController ($scope, $filter)
+{
+    var buy = this;
+    buy.items = [{ name: "cookies", quantity: 10 },
+                 { name: "chips", quantity: 5 },
+                 { name: "pizza", quantity: 7 },
+                 { name: "candy", quantity: 2 },
+                 { name: "nachos", quantity: 1 }
+                ];
+}
+
+AlreadyBoughtController .$inject = ['$scope', '$filter'];
+function AlreadyBoughtController ($scope, $filter)
+{
+    var bought = this;
+    bought.items = [{ name: "ice cream", quantity: 7 },
+                    { name: "soda", quantity: 3 }];
+
+}
+
+})();
