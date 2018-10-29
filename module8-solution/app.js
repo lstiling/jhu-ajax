04a0b59a054cb6aeedf4fe667e21b66d7db82442
Leah Stiling <Leah.Stiling@jhuapl.edu>
2018-10-28 21:58:41 -0400

    Add angularJS and decalred app and controller

---
+++ module8-solution/app.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController ', NarrowItDownController );
+
+NarrowItDownController .$inject = ['$scope', '$filter'];
+function NarrowItDownController ($scope, $filter)
+{
+    var narrowItDown = this;
+}
+
+})();
