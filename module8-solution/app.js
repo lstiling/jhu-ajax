cf207864f2c9d431b0a0ba4f7e7386cbbd588f84
Leah Stiling <Leah.Stiling@jhuapl.edu>
2018-10-28 22:40:09 -0400

    Add menu service that performs http get and returns response in promise

--- module8-solution/app.js
+++ module8-solution/app.js
@@ -2,12 +2,47 @@
 'use strict';
 
 angular.module('NarrowItDownApp', [])
-.controller('NarrowItDownController ', NarrowItDownController );
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
-NarrowItDownController .$inject = ['$scope', '$filter'];
-function NarrowItDownController ($scope, $filter)
+NarrowItDownController.$inject = ['$scope', '$filter', 'MenuSearchService'];
+function NarrowItDownController ($scope, $filter, MenuSearchService)
 {
     var narrowItDown = this;
+    narrowItDown.searchTerm = "";
+
+    narrowItDown.searchMenuItems = function (searchTerm) {
+        var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+        promise.then(function (response) {
+
+            console.log(response.data);
+
+            //var foundItems;
+            //TODO - loop through response for ones that match searchTerm
+            //return foundItems;
+        })
+        .catch(function (error) {
+            console.log(error);
+        })
+    };
+}
+
+MenuSearchService.$inject = ['$http', 'ApiBasePath'];
+function MenuSearchService($http, ApiBasePath)
+{
+    var service = this;
+
+    service.getMatchedMenuItems = function()
+    {
+        var response = $http({
+            method: "GET",
+            url: (ApiBasePath + "/menu_items.json"),
+          });
+      
+        return response;
+    }
 }
 
 })();
