ebcd38cdb928c2a216c34fbe593a7d76e2a3e2a0
Leah Stiling <Leah.Stiling@jhuapl.edu>
2018-12-11 21:56:59 -0500

    Add image and title to my info page

--- module10-solution/src/public/myinfo/myinfo.controller.js
+++ module10-solution/src/public/myinfo/myinfo.controller.js
@@ -15,6 +15,21 @@ function MyInfoController(MenuService) {
   myinfo.email = info['email'];
   myinfo.phone = info['phone'];
   myinfo.dish = info['dish'];
+  myinfo.dishTitle = "";
+  myinfo.dishDescription = "";
+  myinfo.imageUrl = "";
+
+  var promise = MenuService.getMenuItem(myinfo.dish);
+  promise.then(function (response) {   
+    myinfo.dishTitle = response.name;
+    myinfo.dishDescription = response.description;
+    myinfo.imageUrl = "../../../images/menu/" + response.category_short_name + "/" + response.category_short_name + ".jpg";
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+
+ 
 
 
   if(info.firstname == "")
